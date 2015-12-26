53795d9d86352d75a1b8efddde29764795d4c911
Chin Hsu <dblsesame@gmail.com>
2015-12-25 21:41:59 -0500

    try adding InfoWindow and MapMarker to the map.

--- js/app.js
+++ js/app.js
@@ -5,8 +5,23 @@ var map = map || {};
 // center location geocode
 // zoom level (the larger values means higer resol)
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  })
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 21.397, lng: -72.644},
+		zoom: 10
+	});
+	var infoWindow = new google.maps.InfoWindow({map:map});
+
+
+	var myLatLng = {lat:21.783093, lng:-71.764755};
+	infoWindow.setPosition(myLatLng);
+	infoWindow.setContent('Location found.');
+	map.setCenter(myLatLng);
+    //map marker
+
+    var marker = new google.maps.Marker({
+    	position: myLatLng,
+    	title: "My Marker",
+    	map:map
+    });
+    //marker.setMap(map);
 };
